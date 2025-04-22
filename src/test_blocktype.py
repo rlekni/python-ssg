0c89b93cf1b56503342a9e7dab7eccd7b0cedfbc
rlekni <ralekn@pm.me>
2025-04-22 11:42:54 +0100

    block type conversion

---
+++ src/test_blocktype.py
@@ -0,0 +1,22 @@
+import unittest
+from blocktype import BlockType, block_to_block_type
+
+
+class TestBlockTypeConversion(unittest.TestCase):
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+
+if __name__ == "__main__":
+    unittest.main()
